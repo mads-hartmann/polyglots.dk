0a58e3958a58606f28025c4de8374f9662980ab9
Mads Hartmann Jensen <mads379@gmail.com>
2014-09-14 21:45:23 +0200

    Request smaller images from Github

--- site.js
+++ site.js
@@ -8,8 +8,9 @@ $(document).ready(function() {
             var li = $("<li></li>").attr("class", "thumbnail text-center col-md-2 col-sm-2 col-xs-2");
             var link = $("<a></a>").attr("href", element.html_url);
             var username = $("<p>").text(element.login);
-            var image = $("<img>").attr("src", element.avatar_url).attr("class", "img-circle");
-            list.append(li.append(link.append(image).append(username)))
+            var image_url = element.avatar_url + "&size=106"; // default is 460px. No need for so large images.
+            var image = $("<img>").attr("src", image_url).attr("class", "img-circle");
+            list.append(li.append(link.append(image).append(username)));
         });
     });
 
